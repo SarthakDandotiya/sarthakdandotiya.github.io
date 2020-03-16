d95991a9e68335f353e4d72c4f84a2e85d31ee50
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2020-03-16 23:20:30 +0530

    Minor Updates

--- scripts/pointer.js
+++ scripts/pointer.js
@@ -1,40 +1,42 @@
-$(document).on("mousemove", function(e) {
+$(document).on("mousemove", function (e) {
 	$(".pointer").css({
 		left: e.pageX,
-		top: e.pageY - $(document).scrollTop()
+		top: e.pageY - $(document).scrollTop(),
+		display: "block"
 	});
-	$(".clickable").on("mouseenter", function(e) {
+	$(".clickable").on("mouseenter", function (e) {
 		$(".pointer").addClass("clicker", 200);
 	});
-	$(".clickable").on("mouseleave", function(e) {
+	$(".clickable").on("mouseleave", function (e) {
 		$(".pointer").removeClass("clicker");
 	});
-	$(".clickable").on("mousedown", function(e) {
+	$(".clickable").on("mousedown", function (e) {
 		$(".pointer").addClass("mouse-down");
 	});
-	$(".clickable").on("mouseup", function(e) {
+	$(".clickable").on("mouseup", function (e) {
 		$(".pointer").removeClass("mouse-down");
 	});
 });
-$(document).on("mousemove", function(e) {
+$(document).on("mousemove", function (e) {
 	$(".pointer-b").css({
 		left: e.pageX,
-		top: e.pageY - $(document).scrollTop()
+		top: e.pageY - $(document).scrollTop(),
+		display: "block"
 	});
-	$(".clickable").on("mouseenter", function(e) {
+	$(".clickable").on("mouseenter", function (e) {
 		$(".pointer-b")
 			.css("display", "none")
 			.setTimeout(1000);
 	});
-	$(".clickable").on("mouseleave", function(e) {
+	$(".clickable").on("mouseleave", function (e) {
 		$(".pointer-b").css({
 			display: "block"
 		});
 	});
-	// $(".clickable").on("mousedown", function(e) {
-	// 	$(".pointer-b").addClass("mouse-down");
-	// });
-	// $(".clickable").on("mouseup", function(e) {
-	// 	$(".pointer-b").removeClass("mouse-down");
-	// });
+	$(".clickable").on("mousedown", function (e) {
+		$(".pointer-b").addClass("mouse-down");
+	});
+	$(".clickable").on("mouseup", function (e) {
+		$(".pointer-b").removeClass("mouse-down");
+	});
 });
